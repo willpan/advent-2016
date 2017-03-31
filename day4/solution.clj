8cb5d6b489151371cdd1a4ec30492c8cc81948bf
William Pan <will.w.pan@gmail.com>
2017-03-30 23:38:23 -0400

    Add solution to day 4 part b

--- day4/solution.clj
+++ day4/solution.clj
@@ -37,17 +37,49 @@
        (map first)
        (take 5)
        (apply str)))
-; (calc-checksum example) => "jxfin"
+; (-> example parse-data :room-name calc-checksum) => "jxfin"
 
+(def valid-items
+  (->> input 
+       (map parse-data)
+       (filter #(= (:checksum %) (calc-checksum (:room-name %))))))
 
 (def solution-part-1
   "Sum of the sector-ids of real rooms. Room is real if checksum of name
   matches the checksum in the encrypted entry"
-  (->> input
-       (map parse-data)
-       (filter #(= (:checksum %) (calc-checksum (:room-name %))))
+  (->> valid-items
        (map :sector-id)
        (reduce +)))
 
 (println "The solution to part a is:" solution-part-1)
 ; The solution to part a is: 173787
+
+(defn- decrypt-char
+  [c shift]
+  (if (= c \-)
+    \space
+    (char (let [x (int c)
+                shifted (+ x (mod shift 26))]
+            (if (> shifted 122) (- shifted 26) shifted)))))
+
+(defn decrypt-room-name
+  "Decrypts the room name via Ceaser cipher with given shift and replacing
+  - with space."
+  [room-name shift]
+  (apply str (map #(decrypt-char % shift) room-name)))
+
+(defn decrypt-item
+  "Decrypts a single item"
+  [item]
+  (assoc item :room-name (decrypt-room-name (:room-name item)
+                                            (:sector-id item))))
+
+(def solution-part-2
+  (->> valid-items
+       (map decrypt-item)
+       (filter #(= "northpole object storage" (:room-name %)))
+       first
+       :sector-id))
+
+(println "The solution to part 2 is:" solution-part-2)
+; The solution to part 2 is: 548
